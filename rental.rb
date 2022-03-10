58581688d3064db8578f564b6b53c411ee2e9aa5
charlesgobina <charles.gobina@gmail.com>
2022-03-10 17:44:53 +0100

    Contain rental class

---
+++ rental.rb
@@ -0,0 +1,26 @@
+class Rental
+
+  attr_accessor :book, :person
+
+  def initialize(book, person)
+    @book = book
+    @book.rental << self
+    @person = person
+    @person.rental << self
+  end
+
+  def stud_classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
+  end
+
+  def book_rented=(book)
+    @book = book
+    book.rental.push(self) unless book.rental.include?(self)
+  end
+
+  def rented_by=(person)
+    @person = person
+    person.rental.push(self) unless person.rental.include?(self)
+  end
+end
